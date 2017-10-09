0a075f3ae230561f9e14d02347e843801ee53a85
zhonghaoliu <joshualiu94@gmail.com>
2017-10-09 20:54:18 +0000

    step5: make the request

--- download_avatars.js
+++ download_avatars.js
@@ -6,9 +6,25 @@ var GITHUB_USER = "zhonghaoliu";
 var GITHUB_TOKEN = "443f2207a55d6386b72b778abcd6370d9c2f371a";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  // console.log(requestURL);
-}
+  var UA = "GitHub Avatar Downloader - Student Project";
+  var options = {
+    url:'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': UA
+    }
+  };
+  // var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  request(options, function(err, res, body) {
+    console.log("StatusCode: ", res.statusCode);
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('body: ', body);
+    }
+
+  });
+
+};
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
