4e001fdf2cdd4ed2b113006aa30c3f06ee0b41b9
zhonghaoliu <joshualiu94@gmail.com>
2017-10-09 20:37:08 +0000

    step4: create the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "zhonghaoliu";
+var GITHUB_TOKEN = "443f2207a55d6386b72b778abcd6370d9c2f371a";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  // console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
