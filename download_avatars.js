1c418a6fc0a33a510138cb04132e7e6d931aee62
zhonghaoliu <joshualiu94@gmail.com>
2017-10-10 00:03:36 +0000

    confit file for secrets

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,24 @@
+//setup libraries and initial variables
+require('dotenv').config() //Secret keys
 var request = require('request');
 var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = "zhonghaoliu";
-var GITHUB_TOKEN = "443f2207a55d6386b72b778abcd6370d9c2f371a";
 var UA = "GitHub Avatar Downloader - Student Project";
 
-//the token need to be hidden later!!!
-
+//setup the input variables
 var inputOwner = process.argv[2] || 0;
 var inputName = process.argv[3] || 0;
 
+//define the main function
 function getRepoContributors(repoOwner, repoName, cb) {
   if ((repoOwner && repoName) == 0 ) {
     console.log('Invalid Input :/');
     return ;
   }
   var options = {
-    url:'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    url:'https://'+ GITHUB_USER + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'User-Agent': UA
     }
@@ -33,6 +34,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 };
 
+
+//the execute code, contains two input variables and a callback funcion
 getRepoContributors(inputOwner, inputName, function(err, result) {
   if (err) {
     console.log('error: ', err);
@@ -44,8 +47,7 @@ getRepoContributors(inputOwner, inputName, function(err, result) {
 });
 
 function downloadImageByURL(url, filePath) {
-  request.get(url)
-         .on('error', function(err) {
+  request.get(url).on('error', function(err) {
            console.log ('Error: ', err);
          })
          .pipe(fs.createWriteStream(`./pics/${filePath}.jpg`));
